973489319ece1f466c2f41b960eae3e9a486edda
Alejandro <alejandro.ion.carvajal@gmail.com>
2019-07-19 15:47:38 -0400

    add and remove item from list
    add and remove item from list, some front end modifications

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
+var removeSVG = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path class="fill" fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>';
+var completeSVG = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path class="fill" fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>';
+
+
 // add button click if there is any text in input
 document.getElementById('add').addEventListener('click', function(){
     var value = document.getElementById('item').value;
     var placeHolder = document.getElementById('item');
-    if(value){
-        
-    }else{
-        placeHolder.placeholder = "No ha ingresado ningun producto..";
-    }
+    if(value) {
+        addItemTodo(value);
+        document.getElementById('item').value = '';}
 });
+function removeItem(){
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    
+    parent.removeChild(item);
+};
+
+function completeTodo(){
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    console.log(id);
+};
+
+function addItemTodo(text){
+    var list = document.getElementById('todo');
+
+    var item = document.createElement('li'); 
+    item.innerText = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeSVG;
+
+    remove.addEventListener('click', removeItem);
+
+    var complete = document.createElement('button');
+    complete.classList.add('complete');
+    complete.innerHTML = completeSVG;
+
+    complete.addEventListener('click', completeTodo);
+
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    item.appendChild(buttons);
+
+    list.insertBefore(item, list.childNodes[0]);
+
+
+};
